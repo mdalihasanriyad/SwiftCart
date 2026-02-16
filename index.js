891a01f88eb8a0f370ad90c8f25be7882376f90e
mdalihasanriyad <ahriyad.com@gmail.com>
2026-02-16 21:08:57 +0600

    JS file added

---
+++ index.js
@@ -0,0 +1,168 @@
+
+/* =========================
+
+LOAD PRODUCTS
+
+========================= */
+
+async function loadProducts(){
+
+const res = await fetch(
+
+"https://fakestoreapi.com/products"
+
+);
+
+const products = await res.json();
+
+displayProducts(products);
+
+}
+
+
+
+
+/* =========================
+
+DISPLAY PRODUCTS
+
+========================= */
+
+function displayProducts(products){
+
+const container = document.getElementById(
+
+"productContainer"
+
+);
+
+container.innerHTML = "";
+
+
+
+products.forEach(product => {
+
+
+container.innerHTML += `
+
+
+<div class="card bg-base-100 shadow">
+
+
+<figure class="p-6">
+
+<img src="${product.image}"
+
+class="h-40 object-contain"/>
+
+</figure>
+
+
+
+<div class="card-body">
+
+
+<span class="badge badge-primary badge-sm">
+
+${product.category}
+
+</span>
+
+
+
+<h2 class="card-title text-lg">
+
+${product.title.slice(0,30)}
+
+</h2>
+
+
+
+<p class="font-bold text-primary">
+
+$${product.price}
+
+</p>
+
+
+
+<div class="card-actions justify-between">
+
+
+<button onclick="productDetails(${product.id})"
+
+class="btn btn-outline btn-sm">
+
+Details
+
+</button>
+
+
+
+<button onclick="addToCart(${product.id})"
+
+class="btn btn-primary btn-sm">
+
+Add
+
+</button>
+
+
+</div>
+
+
+</div>
+
+
+</div>
+
+
+`;
+
+
+});
+
+}
+
+
+
+
+
+/* =========================
+
+DETAILS
+
+========================= */
+
+function productDetails(id){
+
+alert("Product ID: " + id);
+
+}
+
+
+
+
+/* =========================
+
+ADD TO CART
+
+========================= */
+
+function addToCart(id){
+
+alert("Added Product: " + id);
+
+}
+
+
+
+
+/* =========================
+
+INIT
+
+========================= */
+
+loadProducts();
+
